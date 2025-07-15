3bd79299bf643082a0c214e62c34498257cff7bd
Iftekhar <iftars2774@gmail.com>
2025-07-16 00:28:19 +0530

    few refinements

--- Basics/index.js
+++ Basics/index.js
@@ -4,19 +4,30 @@ console.log("Hello World");
 
 //// --------- Alerts & Prompts-------
 
-alert("Warning, Something is Wrong!")  // can be used after linking to an html file and rendering to browser.
+alert("Warning, Something is Wrong!");  // can be used after linking to an html file and rendering to browser.
 
 console.error('This is an error msg');
 console.warn('This is a warning msg');
 
-let firstName = prompt("Enter your first name: ")
-let lastName = prompt("Enter your last name: ")
-let fullName = firstName + " " + lastName
-console.log(fullName)
+let firstName = prompt("Enter your first name: ");
+let lastName = prompt("Enter your last name: ");
+let fullName = firstName + " " + lastName;
+console.log(fullName);
 
 
 
-let name = prompt("Enter your name: ")
-let age = prompt("Enter your age: ")
-let msg = `${name} is ${age} years old`
-alert(msg)
+let name = prompt("Enter your name: ");
+let age = prompt("Enter your age: ");
+let msg = `${name} is ${age} years old`;
+alert(msg);
+
+
+
+
+//// --------- parseInt-------
+
+let num = prompt("Enter a number: ");
+num = parseInt(num);
+
+
+
