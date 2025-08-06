6a3e22e5a40f87b6a99cb99d3dbde621d4d99ad0
Iftekhar <iftars2774@gmail.com>
2025-08-06 15:18:53 +0530

    refined and structured

--- DOM/app.js
+++ DOM/app.js
@@ -1,274 +1,286 @@
 
-//// ------- getElementById---------
+//// ------------- getElementById ----------------
 
-// let image = document.getElementById("mainImg");
-// console.log(image);
-// console.log(image.src);
-
-
-
-////------ getElementsByClassName --------------
-
-// let smallImages = document.getElementsByClassName("oldImg");
-
-// console.log(smallImages);
-
-// for(let i=0; i<smallImages.length; i++) {
-//     console.dir(smallImages[i].src);
-// }
-
-// for(let i = 0; i<smallImages.length; i++) {
-//     smallImages[i].src = "assets/spiderman_img.png";
-//     console.log(`src for ${i} is changed`);
-// }
+let image = document.getElementById("mainImg");
+console.log(image);
+console.log(image.src);
 
 
 
-//// ------ getElementsByTagName -------------
 
-// let paras = document.getElementsByTagName("p");
-// console.log(paras);
+////--------------- getElementsByClassName ---------------------
 
-//// document.getElementsByTagName("p")[0].innerText = "abc";
+let smallImages = document.getElementsByClassName("oldImg");
+
+console.log(smallImages);
+
+for(let i=0; i<smallImages.length; i++) {
+    console.dir(smallImages[i].src);
+}
+
+for(let i = 0; i<smallImages.length; i++) {
+    smallImages[i].src = "assets/spiderman_img.png";
+    console.log(`src for ${i} is changed`);
+}
 
 
 
-//// ------- Query Selectors -----------
 
-// console.dir(document.querySelector('h1'));
+//// ---------- getElementsByTagName ------------------
 
-// console.dir(document.querySelector("#description"));
+let paras = document.getElementsByTagName("p");
+console.log(paras);
 
-// console.dir(document.querySelector(".oldImg"));
+// document.getElementsByTagName("p")[0].innerText = "abc";
 
-// console.dir(document.querySelector("div a"));
 
-// console.dir(document.querySelectorAll("div a"));
 
-// console.dir(document.querySelectorAll("p"));
+
+//// ---------- Query Selectors -----------
+
+console.dir(document.querySelector('h1'));
+
+console.dir(document.querySelector("#description"));
+
+console.dir(document.querySelector(".oldImg"));
+
+console.dir(document.querySelector("div a"));
+
+console.dir(document.querySelectorAll("div a"));
+
+console.dir(document.querySelectorAll("p"));
+
 
 
 
 //// --------- Setting content in objects -------------
 
-// let para = document.querySelector("p");
-// console.log(para);
+let para = document.querySelector("p");
+console.log(para);
 
-// console.dir(para);
+console.dir(para);
 
-// console.log(para.innerText);
-// console.log(para.innerHTML);
-// console.log(para.textContent);
+console.log(para.innerText);
+console.log(para.innerHTML);
+console.log(para.textContent);
 
-// para.innerText = 'abc';
-// para.innerText = "Hi, I'm Peter Parker!";
+para.innerText = 'abc';
+para.innerText = "Hi, I'm Peter Parker!";
 
-// para.innerText = "Hi, I'm <b> Peter Parker! </b>";
+para.innerText = "Hi, I'm <b> Peter Parker! </b>";
 
-// para.innerHTML = "Hi, I'm <b> Peter Parker! </b>";
+para.innerHTML = "Hi, I'm <b> Peter Parker! </b>";
 
 
-// let heading = document.querySelector("h1");
+let heading = document.querySelector("h1");
 
-// console.log(heading.innerText);
+console.log(heading.innerText);
 
-// heading.innerHTML = "<u>Spider Man </u>";
+heading.innerHTML = "<u>Spider Man </u>";
 
-// heading.innerHTML = `<u>${heading.innerText}</u>`;
+heading.innerHTML = `<u>${heading.innerText}</u>`;
+
 
 
 
 //// ------- Manipulating Attributes -------------
 
-// let img = document.querySelector("img");
+let img = document.querySelector("img");
 
-// console.log(img);
+console.log(img);
 
-// console.log(img.getAttribute('id'));
+console.log(img.getAttribute('id'));
 
-// img.setAttribute('id', "spidermanImg");
+img.setAttribute('id', "spidermanImg");
 
-// console.log(img.getAttribute('class'));
+console.log(img.getAttribute('class'));
 
-// img.setAttribute("class", "spiderman");
+img.setAttribute("class", "spiderman");
 
-// console.log(img.getAttribute("class"));
+console.log(img.getAttribute("class"));
+
 
 
 
 //// ---------- Manipulating style ---------------
 
-// img = document.querySelector("img");
+img = document.querySelector("img");
 
-// console.log(img);
-// console.log(img.style);
+console.log(img);
+console.log(img.style);
 
-// heading = document.querySelector("h1");
+heading = document.querySelector("h1");
 
-// console.log(heading.style);
+console.log(heading.style);
 
-// heading.style.color = "purple";
+heading.style.color = "purple";
 
-// heading.style.backgroundColor = "yellow";
-
-
-// let links = document.querySelectorAll("div a");
-
-// for(let i =0; i<links.length; i++) {
-//     links[i].style.color = "green";
-// }
-
-// for(i of links) {
-//     i.style.color = "green";   // inline style
-// }
+heading.style.backgroundColor = "yellow";
 
 
-// let box = document.querySelector('.box');
+let links = document.querySelectorAll("div a");
 
-// console.log(box.style);  // This method can't access the css styling it only accesses inline styling.
+for(let i =0; i<links.length; i++) {
+    links[i].style.color = "green";
+}
+
+for(i of links) {
+    i.style.color = "green";   // inline style
+}
+
+
+let box = document.querySelector('.box');
+
+console.log(box.style);  // This method can't access the css styling it only accesses inline styling.
+
+
 
 
  //// --------- classList -------------
 
-// img = document.querySelector('img');
+img = document.querySelector('img');
 
-// console.log(img.classList);
-
-
-// heading = document.querySelector("h1");
-
-// console.log(heading.classList);
-
-// heading.classList.add("abc");
-// console.log(heading.classList);
-
-// heading.classList.add("green");
-// heading.classList.add("underline");
-
-// heading.classList.remove("green");
-
-// heading.setAttribute("class", "green");
-
-// console.log(heading.classList);
-
-// console.log(heading.classList.contains("green"));
-// console.log(heading.classList.contains("underline"));
-
-// heading.classList.toggle("green");
-// console.log(heading.classList);
-
-// heading.classList.toggle("underline");
-// console.log(heading.classList);
-
-// heading.classList.toggle("green");
-// console.log(heading.classList);
+console.log(img.classList);
 
 
-// box = document.querySelector(".box");
-// console.log(box.classList);
+heading = document.querySelector("h1");
 
-// box.classList.add("yellowBg");
-// console.log(box.classList);
+console.log(heading.classList);
 
+heading.classList.add("abc");
+console.log(heading.classList);
 
+heading.classList.add("green");
+heading.classList.add("underline");
 
-//// --------- Navigation --------
+heading.classList.remove("green");
 
-// let h4 = document.querySelector('h4');
+heading.setAttribute("class", "green");
 
-// console.log(h4.parentElement);
-// h4.parentElement
+console.log(heading.classList);
 
-// console.log(h4.children);
+console.log(heading.classList.contains("green"));
+console.log(heading.classList.contains("underline"));
 
+heading.classList.toggle("green");
+console.log(heading.classList);
 
-// box = document.querySelector(".box");
+heading.classList.toggle("underline");
+console.log(heading.classList);
 
-// console.log(box.children);
-// console.log(box.childElementCount);
-
-
-// let ul = document.querySelector("ul");
-// console.log(ul.parentElement);
-// console.log(ul.children);
-// console.log(ul.childElementCount);
-
-// console.log(ul.children);
-// console.log(ul.children[0]);
-// console.log(ul.children[1]);
-// console.log(ul.children[2]);
-
-// console.log(ul.children[2].previousElementSibling);
-// console.log(ul.children[1].previousElementSibling);
-// console.log(ul.children[1].nextElementSibling);
+heading.classList.toggle("green");
+console.log(heading.classList);
 
 
-// img = document.querySelector("img");
+box = document.querySelector(".box");
+console.log(box.classList);
 
-// console.log(img.previousElementSibling);
-// console.log(img.previousElementSibling.style);
+box.classList.add("yellowBg");
+console.log(box.classList);
 
-// img.previousElementSibling.style.color = "green";
+
+
+
+//// --------- Navigation ------------
+
+let h4 = document.querySelector('h4');
+
+console.log(h4.parentElement);
+h4.parentElement
+
+console.log(h4.children);
+
+
+box = document.querySelector(".box");
+
+console.log(box.children);
+console.log(box.childElementCount);
+
+
+let ul = document.querySelector("ul");
+console.log(ul.parentElement);
+console.log(ul.children);
+console.log(ul.childElementCount);
+
+console.log(ul.children);
+console.log(ul.children[0]);
+console.log(ul.children[1]);
+console.log(ul.children[2]);
+
+console.log(ul.children[2].previousElementSibling);
+console.log(ul.children[1].previousElementSibling);
+console.log(ul.children[1].nextElementSibling);
+
+
+img = document.querySelector("img");
+
+console.log(img.previousElementSibling);
+console.log(img.previousElementSibling.style);
+
+img.previousElementSibling.style.color = "green";
+
 
 
 
 ////--------- Adding Elements on page -------------
 
-// let newP = document.createElement("p");
+let newP = document.createElement("p");
 
-// console.log(newP);
+console.log(newP);
 
-// newP.innerText = "Hi, I'm a new p."
+newP.innerText = "Hi, I'm a new p."
 
-// console.log(newP);
-// console.log(newP.innerText);
+console.log(newP);
+console.log(newP.innerText);
 
-// let body = document.querySelector('body');
+let body = document.querySelector('body');
 
-// body.appendChild(newP);
+body.appendChild(newP);
 
-// box = document.querySelector(".box");
-// box.appendChild(newP);
-
-
-// let btn = document.createElement('button');
-// btn.innerText = "Click Me!";
-
-// box = document.querySelector(".box");
-// box.appendChild(btn);
+box = document.querySelector(".box");
+box.appendChild(newP);
 
 
-// newP.append("This is new text.");
-// newP.append(btn);
-// newP.append("Do not click this button.");
+let btn = document.createElement('button');
+btn.innerText = "Click Me!";
+
+box = document.querySelector(".box");
+box.appendChild(btn);
 
 
-// box.prepend(newP);
+newP.append("This is new text.");
+newP.append(btn);
+newP.append("Do not click this button.");
+
+box.prepend(newP);
 
 
 
-// let btn = document.createElement('button');
-// btn.innerText = "NEW BUTTON!!!";
+let btn = document.createElement('button');
+btn.innerText = "NEW BUTTON!!!";
 
-// p = document.querySelector("p");
+p = document.querySelector("p");
 
-// p.insertAdjacentElement('beforebegin', btn);
-// p.insertAdjacentElement('afterbegin', btn);
-// p.insertAdjacentElement('beforeend', btn);
-// p.insertAdjacentElement('afterend', btn);
+p.insertAdjacentElement('beforebegin', btn);
+p.insertAdjacentElement('afterbegin', btn);
+p.insertAdjacentElement('beforeend', btn);
+p.insertAdjacentElement('afterend', btn);
+
+
 
 
 //// ------- Removing Elements -------------
 
-// body = document.querySelector('body');
-// body.removeChild(btn);
+body = document.querySelector('body');
+body.removeChild(btn);
 
-// p.insertAdjacentElement('afterend', btn);
-// btn.remove;
+p.insertAdjacentElement('afterend', btn);
+btn.remove;
 
 
 
-//// --------- Practice Qs ----------
+
+//// --------- Practice Qs -----------
 
 body = document.querySelector('body');
 
@@ -305,6 +317,7 @@ div.appendChild(para1);
 div.classList.add("box2");
 
 
+
 //// Qsl. Create a new input and button element on the page using JavaScript only. 
 // Set the text of button to "Click me'
 
@@ -319,6 +332,7 @@ body.appendChild(input);
 body.appendChild(btn3);
 
 
+
 ////Qs2. Add following attributes to the element :
 //// Change placeholder value of input to "username"
 //// Change the id of button to "btn'.
@@ -326,6 +340,7 @@ body.appendChild(btn3);
 input.setAttribute('placeholder', "username");
 
 btn3.setAttribute('id','btn');
+
 
 
 //// Qs3. Access the btn using the querySelector and button id. 
