79f7aae369c6d5c285bd3147874d72db830656b0
Iftekhar <iftars2774@gmail.com>
2025-08-10 01:25:50 +0530

    refined

--- simonSaysGame/app.js
+++ simonSaysGame/app.js
@@ -7,32 +7,33 @@ let btns = ["red", "yellow", "green", "purple"];
 let started = false;
 let level = 0;
 
-let h2 = document.querySelector("h2")
+let h2 = document.querySelector("h2");
+
 
 document.addEventListener("keypress", function() {
     if(started == false) {
         console.log("started");
         started = true;
     }
-
     levelUp();
 });
+
 
 function gameFlash(btn) {
     btn.classList.add('flash');
     setTimeout(function() {
         btn.classList.remove("flash");
-    }, 250)
-
+    }, 250);
 }
+
 
 function userFlash(btn) {
     btn.classList.add('userFlash');
     setTimeout(function() {
         btn.classList.remove("userFlash");
-    }, 250)
-
+    }, 250);
 }
+
 
 function levelUp() {
     userSeq = [];
@@ -46,8 +47,8 @@ function levelUp() {
     gameSeq.push(randColor);
     console.log(gameSeq);
     gameFlash(randBtn);
-
 }
+
 
 function checkAns(idx) {
 
@@ -65,6 +66,7 @@ function checkAns(idx) {
     }
 }
 
+
 function btnPress() {
     let btn = this;
     userFlash(btn);
@@ -75,10 +77,12 @@ function btnPress() {
     checkAns(userSeq.length - 1);
 }
 
+
 let allBtns = document.querySelectorAll(".btn");
 for (btn of allBtns) {
     btn.addEventListener("click", btnPress);
 }
+
 
 function reset() {
     started = false;
