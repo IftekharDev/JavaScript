5bb413980175f14f56bd81226a3fe8460659be4d
Iftekhar <iftars2774@gmail.com>
2025-08-30 21:23:27 +0530

    uncommented and formatted the code

--- nodejs/02script.js
+++ nodejs/02script.js
@@ -6,34 +6,38 @@
 // const g = 9.8;
 // const PI = 3.14;
 
-// let obj = {
-//     sum:sum,
-//     mul:mul,
-//     g:g,
-//     PI:PI
-// }
+let obj = {
+    sum:sum,
+    mul:mul,
+    g:g,
+    PI:PI
+}
 
-// module.exports = obj;
-
-
-// module.exports = {
-//     sum:sum,
-//     mul:mul,
-//     g:g,
-//     PI:PI
-// }
+module.exports = obj;
 
 
-// module.exports.sum = (a, b) => a+b;
-// module.exports.mul = (a, b) => a*b;
-// module.exports.g = 9.8;
-// module.exports.PI = 3.14;
+////-------------------------------------
+
+module.exports = {
+    sum:sum,
+    mul:mul,
+    g:g,
+    PI:PI
+}
 
 
-// exports.sum = (a, b) => a+b;
-// exports.mul = (a, b) => a*b;
-// exports.g = 9.8;
-// exports.PI = 3.14;
+module.exports.sum = (a, b) => a+b;
+module.exports.mul = (a, b) => a*b;
+module.exports.g = 9.8;
+module.exports.PI = 3.14;
+
+
+exports.sum = (a, b) => a+b;
+exports.mul = (a, b) => a*b;
+exports.g = 9.8;
+exports.PI = 3.14;
+
+
 
 
 //// -------------Export and Import in files via import statement-----------------------
