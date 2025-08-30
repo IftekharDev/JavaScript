bbe2dd9f52e305b75fccc808261b699bcd03dd90
Iftekhar <iftars2774@gmail.com>
2025-08-30 21:20:44 +0530

    uncommented and formatted the codes

--- nodejs/01script.js
+++ nodejs/01script.js
@@ -1,27 +1,33 @@
 
 //// ---------- Process In Node--------------------
 
-// console.log(process.argv);
+console.log(process.argv);
 
-// let args = process.argv;   //node script.js Arsalan Aman Noor Arshiya  (takes input as an array)
+let args = process.argv;   //node script.js Arsalan Aman Noor Arshiya  (takes input as an array)
 
-// for(let i = 2; i<args.length; i++) {
-//     console.log("hello ",args[i] );         // first 2 values are paths
-// }
+for(let i = 2; i<args.length; i++) {
+    console.log("hello ",args[i] );         // first 2 values are paths
+}
+
+
 
 
 //// -------------Export and Import in files-----------------------
 
-// const math = require("./02script");
-// console.log(math);
+const math = require("./02script");
+console.log(math);
 
-// console.log(math.sum(2,5));
+console.log(math.sum(2,5));
+
+
 
 
 //// -------------Export and Import in Directories-----------------------
 
-// const info = require("./Fruits")
-// console.log(info);
+const info = require("./Fruits")
+console.log(info);
+
+
 
 
 //// -------------Export and Import in files via import statement-----------------------
