0c76251604fa887473e697a704277f9fada8ddcd
Iftekhar <iftars2774@gmail.com>
2025-08-30 20:03:23 +0530

    Process in node, export and import in files and directories completed

---
+++ nodejs/01script.js
@@ -0,0 +1,24 @@
+
+//// ---------- Process In Node--------------------
+
+// console.log(process.argv);
+
+// let args = process.argv;   //node script.js Arsalan Aman Noor Arshiya  (takes input as an array)
+
+// for(let i = 2; i<args.length; i++) {
+//     console.log("hello ",args[i] );         // first 2 values are paths
+// }
+
+
+//// -------------Export and Import in files-----------------------
+
+// const math = require("./02script");
+// console.log(math);
+
+// console.log(math.sum(2,5));
+
+
+//// -------------Export and Import in Directories-----------------------
+
+// const info = require("./Fruits")
+// console.log(info);
