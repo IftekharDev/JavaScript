6dee836d8a49a3e26e9393b36ef826bd518b27af
Iftekhar <iftars2774@gmail.com>
2025-09-06 01:07:15 +0530

    Code for create new post

--- REST/index.js
+++ REST/index.js
@@ -9,13 +9,38 @@ app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username : "arsalan",
+        content : "I love coding!"
+    },
+    {
+        username : "zainab",
+        content : "Hard work is so important"
+    },
+    {
+        username : "Harry",
+        content : "Just sleep well, nothing else matters"
+    }
+]
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
 
-app.get("/", (req, res) => {
-    res.send("Server working well!")
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", {posts});
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs")
+});
+
+app.post("/posts", (req, res) => {
+    let {username , content} = req.body;
+    posts.push({username, content});
+    res.send("Post request working");
 });
 
