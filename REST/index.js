f718cbaed7d7acc35d06d96e2ad57aed1fa92bdf
Iftekhar <iftars2774@gmail.com>
2025-09-06 03:12:53 +0530

    delete api implemented

--- REST/index.js
+++ REST/index.js
@@ -70,3 +70,10 @@ app.get("/posts/:id/edit", (req, res) => {
     let post = posts.find((p) => id === p.id);
     res.render("edit.ejs", {post});
 });
+
+app.delete("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    posts = posts.filter((p) => id != p.id);
+    res.redirect("/posts");
+})
