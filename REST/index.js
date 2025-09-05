56ba73c6414eafad4e2aa36aef903fbe4ecf1bc6
Iftekhar <iftars2774@gmail.com>
2025-09-06 02:18:12 +0530

    uuid installed and setuped

--- REST/index.js
+++ REST/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const { v4 : uuidv4 } = require("uuid");
 
 app.use(express.urlencoded({extended: true}));
 
@@ -13,17 +14,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
-        id : "1a",
+        id : uuidv4(),
         username : "arsalan",
         content : "I love coding!"
     },
     {
-        id : "2b",
+        id : uuidv4(),
         username : "zainab",
         content : "Hard work is so important"
     },
     {
-        id : "3c",
+        id : uuidv4(),
         username : "Harry",
         content : "Just sleep well, nothing else matters"
     }
@@ -43,7 +44,8 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
     let {username , content} = req.body;
-    posts.push({username, content});
+    let id = uuidv4();
+    posts.push({id, username, content});
     res.redirect("/posts");
 });
 
