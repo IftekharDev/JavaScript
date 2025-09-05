246af461deabc3b2f1ae14c41f8e28d6cb475345
Iftekhar <iftars2774@gmail.com>
2025-09-05 23:26:41 +0530

    Server started for rest api

--- REST/index.js
+++ REST/index.js
@@ -2,8 +2,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.set(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Server working well!")
 });
 
